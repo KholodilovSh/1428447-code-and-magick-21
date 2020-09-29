dd35fd1ad3fbc8dfe56144123418a0f3eb2fdd92
Холодилов Александр <shurikhol@mail.ru>
2020-09-29 22:56:01 +0300

    Доработка после консультации

--- js/stat.js
+++ js/stat.js
@@ -6,64 +6,64 @@
 //    так и неправильным нарисованным с помощью методов
 //          beginPath, moveTo, closePath, fill и других.
 
-var CLOUD_COLOR = '#fff';
+const CLOUD_COLOR = `#fff`;
 // координаты облака
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
 // размеры облака
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
 
 // Под облаком должна располагаться тень:
 //    многоугольник такой же формы, залитый цветом rgba(0, 0, 0, 0.7) (полупрозрачный чёрный),
 //    смещённый относительно белого на 10px вниз и вправо.
-var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-var SHADOW_GAP = 10;
+const SHADOW_COLOR = `rgba(0, 0, 0, 0.7)`;
+const SHADOW_GAP = 10;
 
 // На облаке должен быть отрисован текст сообщения
 //    ’Ура вы победили! \nСписок результатов: ’ с помощью метода fillText.
 //    Текст должен быть набран шрифтом PT Mono размером 16px.
-var FONT_FAMILY = '16px "PT Mono"';
-var FONT_COLOR = '#000';
-var TEXT_X = 30;
-var TEXT_Y = 30;
-var TEXT_GAP = 20;
+const FONT_FAMILY = `16px "PT Mono"`;
+const FONT_COLOR = `#000`;
+const TEXT_X = 30;
+const TEXT_Y = 30;
+const TEXT_GAP = 20;
 
 // Параметры гистограммы:
 // Высота гистограммы 150px.
-var GIST_HEIGHT = 150;
-var GIST_X = 50;
-var GIST_Y = CLOUD_Y + CLOUD_HEIGHT - GIST_HEIGHT - 40;
+const GIST_HEIGHT = 150;
+const GIST_X = 50;
+const GIST_Y = CLOUD_Y + CLOUD_HEIGHT - GIST_HEIGHT - 40;
 // Ширина колонки 40px.
-var BAR_WIDTH = 40;
+const BAR_WIDTH = 40;
 // Расстояние между колонками 50px.
-var BAR_GAP = 50;
+const BAR_GAP = 50;
 // Цвет колонки игрока Вы rgba(255, 0, 0, 1).
-var MY_COLOR = 'rgba(255, 0, 0, 1)';
+const MY_COLOR = `rgba(255, 0, 0, 1)`;
 // Цвет колонок других игроков — синий,
 //      а насыщенность задаётся случайным образом.
-var GIST_HSL_COLOR = 'hsl(240,'
-var GIST_HSL_LIGHTNESS = '50%)'
+const GIST_HSL_COLOR = `hsl(240,`;
+const GIST_HSL_LIGHTNESS = `50%)`;
 // Времена игроков располагаются над колонками.
-var TIME_GAP = 10;
+const TIME_GAP = 10;
 // Имена игроков — под колонками гистограммы.
-var PLAYERS_Y = CLOUD_Y + CLOUD_HEIGHT - 20;
+const PLAYERS_Y = CLOUD_Y + CLOUD_HEIGHT - 20;
 
-var renderCloud = function(ctx, x, y, color) {
+const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
-  var maxElement = arr[0];
+const getMaxElement = function (arr) {
+  let maxElement = arr[0];
 
-  for (var i = 1; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
-    	maxElement = arr[i];
+      maxElement = arr[i];
     }
   }
   return maxElement;
-}
+};
 
 // renderStatistics - метод объекта window
 //    со следующими аргументами:
@@ -74,46 +74,45 @@ var getMaxElement = function(arr) {
 //            Массив содержит время прохождения уровня соответствующего игрока из массива names.
 //            Время прохождения уровня задано в миллисекундах.
 
-window.renderStatistics = function(ctx, players, times) {
-	// тень
+window.renderStatistics = function (ctx, players, times) {
+  // тень
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, SHADOW_COLOR);
-	// основа = облако
-	renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+  // основа = облако
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
   ctx.fillStyle = FONT_COLOR;
   ctx.font = FONT_FAMILY;
-  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_X, CLOUD_Y + TEXT_Y);
-  ctx.fillText('Список результатов: ', CLOUD_X + TEXT_X, CLOUD_Y + TEXT_X + TEXT_GAP);
+  ctx.fillText(`Ура вы победили!`, CLOUD_X + TEXT_X, CLOUD_Y + TEXT_Y);
+  ctx.fillText(`Список результатов: `, CLOUD_X + TEXT_X, CLOUD_Y + TEXT_X + TEXT_GAP);
 
-  var maxTime = getMaxElement(times);
-  var barHeight;
+  let maxTime = getMaxElement(times);
 
-	for (var i = 0; i < players.length; i++) {
+
+  for (let i = 0; i < players.length; i++) {
+    let barHeight = GIST_HEIGHT * times[i] / maxTime;
+    let barNextX = CLOUD_X + GIST_X + (BAR_WIDTH + BAR_GAP) * i;
+
     ctx.fillStyle = FONT_COLOR;
-	 	ctx.fillText(players[i],
-	  	CLOUD_X + GIST_X + (BAR_WIDTH + BAR_GAP)*i,
-	 		PLAYERS_Y
+    ctx.fillText(players[i],
+        barNextX,
+        PLAYERS_Y
     );
-
-    barHeight = GIST_HEIGHT * times[i] / maxTime;
 
     ctx.fillText(Math.round(times[i]),
-	  	CLOUD_X + GIST_X + (BAR_WIDTH + BAR_GAP)*i,
-      GIST_Y + GIST_HEIGHT - barHeight - TIME_GAP,
+        barNextX,
+        GIST_Y + GIST_HEIGHT - barHeight - TIME_GAP
     );
 
-    if (players[i] === "Вы") {
-      ctx.fillStyle = MY_COLOR;
-    }
-    else {
-      ctx.fillStyle = GIST_HSL_COLOR + 100* Math.random() + '%,' + GIST_HSL_LIGHTNESS;
-    }
+    ctx.fillStyle = (players[i] === `Вы`) ? (MY_COLOR) :
+      (GIST_HSL_COLOR + randomNumber() + `%,` + GIST_HSL_LIGHTNESS);
 
-		ctx.fillRect(
-			CLOUD_X + GIST_X + (BAR_WIDTH + BAR_GAP)*i,
-      GIST_Y + GIST_HEIGHT - barHeight,
-      BAR_WIDTH,
-			barHeight
-		);
+    ctx.fillRect(
+        barNextX,
+        GIST_Y + GIST_HEIGHT - barHeight,
+        BAR_WIDTH,
+        barHeight
+    );
   }
-}
+};
+
+const randomNumber = () => Math.round(100 * Math.random());
